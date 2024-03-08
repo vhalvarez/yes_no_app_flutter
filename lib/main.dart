9abcb2330c8f1f6c6f652d01453d367dbc10e3cb
Vhalvarez <alvarez3197@gmail.com>
2024-03-08 19:03:31 -0400

    ListView - area de los mensajes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,19 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Yes No App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor:  0).theme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: FilledButton(
-            onPressed: () {}, 
-            child: const Text('Click Me')),
-        ),
-      ),
-    );
+        title: 'Yes No App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 0).theme(),
+        home: const ChatScreen());
   }
 }
---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://imagenes.elpais.com/resizer/sk0ZF93ts8UOTb3exVVFNIrkGDA=/1960x1470/cloudfront-eu-central-1.images.arcpublishing.com/prisa/H2FJ6NCONVPDNI6W7O5FTK6EM4.jpg'),
+          ),
+        ),
+        title: const Text("Amor <3"),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return Text('Indice: $index');
+                    }))
+          ],
+        ),
+      ),
+    );
+  }
+}
