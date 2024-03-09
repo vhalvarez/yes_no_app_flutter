c96c5da18c8b2cde9be322b0ea5157a0511b2cdc
Vhalvarez <alvarez3197@gmail.com>
2024-03-09 09:50:41 -0400

    Mostrar mensaje mientras se carga la imagen

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -43,6 +43,16 @@ class _ImageBubble extends StatelessWidget {
             'https://yesno.wtf/assets/yes/10-271c872c91cd72c1e38e72d2f8eda676.gif',
             width: size.width * 0.7,
             height: 150,
-            fit: BoxFit.cover,));
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if( loadingProgress == null ) return child;
+
+              return Container(
+                width: size.width * 0.7,
+                height: 150,
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: const Text("Loading..."),
+              );
+            },));
   }
 }
