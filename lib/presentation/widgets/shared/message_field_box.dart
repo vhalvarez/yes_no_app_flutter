26e45201bd18078d6ca533b14dada8ef84d0d583
Vhalvarez <alvarez3197@gmail.com>
2024-03-09 10:33:59 -0400

    Comportamiento del form field

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -3,34 +3,40 @@ import 'package:flutter/material.dart';
 class MessageFieldBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
+    final textControlLer = TextEditingController();
+    final focusNode = FocusNode();
 
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
-        borderRadius: BorderRadius.circular(40)
-    );
+        borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
-        enabledBorder: outlineInputBorder,
-        focusedBorder: outlineInputBorder,
-        filled: true,
-        suffixIcon: IconButton(
-          icon: const Icon(Icons.send_outlined),
-          onPressed: () {
-            print('Valor de la caja de texto?');
-          },
-        ),
-      );
-    
+      hintText: 'End your message with a "??"',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {
+          final textValue = textControlLer.value.text;
+          print('Valor de la caja de texto?');
+          textControlLer.clear();
+        },
+      ),
+    );
 
     return TextFormField(
-        decoration: inputDecoration,
-        onFieldSubmitted: (value) {
-          print('Submit value $value');
-        },
-        onChanged: (value) {
-          print('changed: $value');
-        },
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textControlLer,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Submit value $value');
+        textControlLer.clear();
+        focusNode.requestFocus();
+      },
     );
   }
 }
